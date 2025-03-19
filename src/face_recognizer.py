1abf95f7dcf25fbe731807ad5079c3fd18e3c96a
Raisun Lakra <raisunlakra18@gmail.com>
2025-03-19 16:23:28 +0530

    Updated the code to close window and resize the image

--- src/face_recognizer.py
+++ src/face_recognizer.py
@@ -2,6 +2,8 @@ import cv2
 import face_recognition
 import numpy as np
 
+window_name = "Live Video - Face Recognition"
+
 def recognize_faces(frame, data):
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
@@ -27,4 +29,17 @@ def recognize_faces(frame, data):
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
         cv2.putText(frame, label, (left, top - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
 
-    return frame
+    # Show image with face recognition
+    while True:
+        frame_resized = cv2.resize(frame, (int(frame.shape[1] * 1.5), int(frame.shape[0] * 1.5)))
+        # cv2.imshow("Recognized Faces", frame)
+        cv2.imshow("Recognized Faces", frame_resized)
+        if cv2.waitKey(1) & 0xFF == ord('q'):  # Press 'q' to exit
+            print("Window closed")
+            break
+        # if cv2.getWindowProperty(window_name, cv2.WND_PROP_VISIBLE) < 0:
+        #     print("Press q to close window.")
+        # if cv2.waitKey(1) & 0xFF == ord('q') or cv2.getWindowProperty(window_name, cv2.WND_PROP_VISIBLE) < 1:
+        #     break
+
+    cv2.destroyAllWindows()
